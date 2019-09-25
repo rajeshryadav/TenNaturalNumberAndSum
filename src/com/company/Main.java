cd361b04c75116854a4ece6739da795f9c12547a
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-25 13:27:09 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        int sum =0;
+
+        for (i=1;i<=10;i++ )
+            sum=sum+i;
+        System.out.println(sum);
+
+    }
+}
